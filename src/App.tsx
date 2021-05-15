fdddb0ae92b26a6f131b639b81a26304c72f5696
reversonthaiam <reversonthayan@gmail.com>
2021-05-15 14:02:32 -0300

    projeto configurado pronto para começar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import { GlobalStyle} from './styles/global'
 
 export function App() {
   return (
     <div className="App">
-      <h1>hello word</h1>
+      <h1 className="title">hello word</h1>
+      <GlobalStyle></GlobalStyle>
     </div>
   );
 }
-
-
