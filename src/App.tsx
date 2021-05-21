892a57ad6e8fbdf19b619a9bb8ed80940bb77d61
Reverson <reverson@pagprime.com.br>
2021-05-21 17:23:05 -0300

    fim dia

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
-import Modal from "react-modal";
+
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsContext, TransactionsProvider} from './TransactionsContext'
+import { TransactionsProvider} from './TransactionsContext'
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
